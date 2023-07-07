8b8cfb51faad2dcf3f6626bd142f50d467fcba66
Balolebwami <tchasingajacques@gmail.com>
2023-07-07 16:26:01 +0200

    new commit

---
+++ src/modules/CardCart.js
@@ -0,0 +1,8 @@
+
+function displayNumberOfCard(){
+    const getNumberOfCard =  document.querySelector('.CardNumber')
+const card = document.querySelectorAll('.card')
+getNumberOfCard.innerHTML = `( ${card.length} )`
+}
+
+export default displayNumberOfCard;
