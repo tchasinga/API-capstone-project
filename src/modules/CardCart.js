3d0ee0bb680a9c87a91abb2337a95bd8e01635e7
Henok Kirubel <henokhm2@gmail.com>
2023-07-08 23:05:46 +0300

    Use ES6 arrow functions instead of function keyword

--- src/modules/CardCart.js
+++ src/modules/CardCart.js
@@ -1,7 +1,7 @@
-function displayNumberOfCard() {
+const displayNumberOfCard = () => {
   const getNumberOfCard = document.querySelector('.CardNumber');
   const card = document.querySelectorAll('.card');
   getNumberOfCard.innerHTML = `( ${card.length} )`;
-}
+};
 
 export default displayNumberOfCard;
